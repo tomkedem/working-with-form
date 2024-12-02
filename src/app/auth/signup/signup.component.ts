f775187f20a5a8600b83e3c30eb5cf79f8771230
Tomer Kedem <tompha1@hotmail.com>
2024-12-02 10:08:34 +0200

    Exercise

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('',{
+      validators: [ Validators.email, Validators.required]
+    }),
+    password: new FormControl('',{
+      validators: [ Validators.required, Validators.minLength(6)]      
+    }),
+  });
+  onSubmit(){
+    console.log('myForm', this.form);
+    const enteredEmail =  this.form.value.email;
+    const enteredPassword =  this.form.value.password;
+    console.log(enteredPassword + ',' ,enteredEmail);
+    
+  }
+  onReset(){
+    this.form.reset();
+  }
+}
